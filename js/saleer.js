4aef89719fafafd46247006c00fb6e0da45249aa
saleer-dev <34818296+saleer-dev@users.noreply.github.com>
2017-12-26 18:38:06 +0300

    Update saleer.js

--- js/saleer.js
+++ js/saleer.js
@@ -1,1 +1,22 @@
+setTimeout(function () {
+        $('.section1').css('height', window.innerHeight + 'px');
+        $(window).scrollTop(0);
+    }, 50);
+
+$(window).on('resize', function() {
+        $(window).scrollTop(0);
+        $('.section1').css('height', window.innerHeight + 'px');
+
+        setTimeout(function() {
+            $('.section1').css('height', window.innerHeight + 'px');
+            $(window).scrollTop(0);
+        }, 400);
+    });
+
+$('.action_button_new').on('touchstart', function(e) {  
+    $(this).css({"background-color":"#d38c3b"});
+});
+$('.action_button_new').on('touchend', function(e) {  
+    $(this).css({"background-color":"#F09833"});
+});
 
