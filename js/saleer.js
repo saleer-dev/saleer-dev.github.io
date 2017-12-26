ec5f2990c61b5fe765053adbb3b64d84980d22c4
saleer-dev <34818296+saleer-dev@users.noreply.github.com>
2017-12-26 19:14:37 +0300

    Update saleer.js

--- js/saleer.js
+++ js/saleer.js
@@ -14,7 +14,7 @@ $(window).on('resize', function() {
     });
 
 $('a.openInTelegram').on('touchstart', function(e) {  
-    $(this).css({"opacity":"0.75";"box-shadow":"none"});
+    $(this).css({"opacity":"0.7";"box-shadow":"none"});
 });
 $('a.openInTelegram').on('touchend', function(e) {  
     $(this).css({"opacity":"1";"box-shadow":"0 4px 10px 1px rgba(0, 0, 0, 0.14)"});
